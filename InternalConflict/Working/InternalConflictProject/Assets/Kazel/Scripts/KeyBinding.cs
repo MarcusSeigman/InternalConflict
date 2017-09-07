a5c5df3bc8d1e8d03f9e6fd405bdccf58d6c5cbd
Kazel <kazel123@live.com>
2017-09-07 15:34:39 -0400

    I am commiting the HUD

---
+++ InternalConflict/Working/InternalConflictProject/Assets/Kazel/Scripts/KeyBinding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBinding : MonoBehaviour {
+
+    public Button EAbility;
+
+  
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) { }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) { }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) { }
+        else if (Input.GetKeyDown(KeyCode.Alpha5)) { }
+        else if (Input.GetKeyDown(KeyCode.E)) { }
+        else if (Input.GetKeyDown(KeyCode.R)) { }
+        else if (Input.GetKeyDown(KeyCode.F)) { }
+        else if (Input.GetKeyDown(KeyCode.C)) { }
+    }
+
+}
